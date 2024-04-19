38b69ea38beece49414ce309ccef741a7f7a8165
Michael Wyss <michael.wyss@nexplore.ch>
2024-04-19 15:47:58 +0200

    init cycleMap

--- 02_CSharp/02_ASP.NET App/Lernende.GrooveBase2.0/Lernende.GrooveBase2.0/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Lernende.GrooveBase2._0.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
